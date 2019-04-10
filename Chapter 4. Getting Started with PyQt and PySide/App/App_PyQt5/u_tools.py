7d755af014a580b8fd5dac22bbc0d466f516e4c2
Volodymyr Kirichinets <webmamoffice@gmail.com>
2019-04-10 16:19:12 +0300

    Add files via upload

---
+++ Chapter 4. Getting Started with PyQt and PySide/App/App_PyQt5/u_tools.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-         #   PYTHON 3   #
+class UTools(object):                          # Class for the functionality.
+
+    def __init__(self):                        # Constructor of the class.
+        self.us1 = "Application with Python."  # String variable.
+
+
+if __name__ == "__main__":                     # Instruction to run the file.
+    ut = UTools()                              # Class instance, will run.
---
+++ Chapter 4. Getting Started with PyQt and PySide/App/App_PySide2/u_style.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-         #   PYTHON 2   #
+from PySide2 import QtWidgets, QtCore, QtGui   # Import of the PyQt5 modules.
+
+color = ["rgba(0,41,59,140)", "rgba(0,41,59,255)"]
+
+
+class UWid(QtWidgets.QWidget):                 # Class for QWidget.
+
+    def __init__(self, parent=None):           # Constructor of the class and
+        super(UWid, self).__init__(parent)     # widget initialization.
+        self.setWindowTitle("U App")           # Title of the application.
+        self.setStyleSheet("background-color: %s;" % (color[0],))

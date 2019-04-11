dc34b147b43f5c78c982f5e9eca62ee760a47fee
Volodymyr Kirichinets <webmamoffice@gmail.com>
2019-04-12 00:53:17 +0300

    Delete cpp_file5.cpp

--- Chapter 1. Introduction to Python and Qt/App/cpp_file5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-void printstr() {             // Define void type of function.
-	cout << "C++ Program\n";  // Print result.
-}
-int main() {
-	printstr();               // Call the function.
-	return 0;
-}

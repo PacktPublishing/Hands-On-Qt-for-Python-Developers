47596e61e437a0b377efe9381a5dae0273be790d
Volodymyr Kirichinets <webmamoffice@gmail.com>
2020-01-02 13:53:17 +0200

    Delete u_tools.py

--- Chapter13/App/App_PySide2/u_tools.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-         #   PYTHON 2   #
-class UTools(object):                          # Class with functionalities.
-
-    def __init__(self):                        # Constructor of the class.
-        self.us1 = "Application with Python."  # String variable.
-
-
-if __name__ == "__main__":                     # Instruction when run file.
-    ut = UTools()                              # Class instance, will run.

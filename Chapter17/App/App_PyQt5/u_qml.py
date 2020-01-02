dcdb199db2bd884e00a4c981de589c8633de597f
Volodymyr Kirichinets <webmamoffice@gmail.com>
2020-01-02 12:47:00 +0200

    Delete u_qml.py

--- Chapter17/App/App_PyQt5/u_qml.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-         #   PYTHON 3   #
-from PyQt5 import QtWidgets, QtCore            # Importing for app, url.
-from PyQt5.QtQml import QQmlApplicationEngine  # Importing the QML engine.
-import sys                                     # The sys module for the app.
-
-app = QtWidgets.QApplication(sys.argv)         # Creating application.
-qwid = QQmlApplicationEngine()                 # QML app engine instance.
-qwid.load(QtCore.QUrl('u_qml.qml'))            # Loads the QML file with URL.
-sys.exit(app.exec_())                          # Executes the application.
